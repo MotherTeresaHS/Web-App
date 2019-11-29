e445ba3f81a58322e867bb917a2725fba47fdd48
Mr. Coxall <mr.coxall@mths.ca>
2019-11-29 02:12:02 +0000

    add cognito signin page

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_NEm4KpQy1', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '1vj0ibiivfq2jm102lr43bgdql'
+    },
+};
+
