eb640c2311fd022cc9ad11da06b249a68f62eb40
Mr. Coxall <mr.coxall@mths.ca>
2019-11-29 04:11:11 +0000

    wildrydes code added

---
+++ WebApp/.debug/helloWorld/.~c9_invoke_sLwhsX.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    #boto3.setup_default_session(region_name='us-east-1')
+    #identity = boto3.client('cognito-identity', region_name='us-east-1')
+    
+    #account_id='XXXXXXXXXXXXXXX'
+    #identity_pool_id='us-east-1_NEm4KpQy1'
+    #api_prefix='ZZZZZZZZZ'
+    
+    #response = identity.get_id(AccountId=account_id, IdentityPoolId=identity_pool_id)
+    #identity_id = response['IdentityId']
+    #print ("Identity ID: %s"%identity_id)
+    
+    # The output from a Lambda proxy integration must be 
+    # in the following JSON object. The 'headers' property 
+    # is for custom response headers in addition to standard 
+    # ones. The 'body' property  must be a JSON string. For 
+    # base64-encoded payload, you must also set the 'isBase64Encoded'
+    # property to 'true'.
+    
+    cognito = boto3.client('cognito-identity')
+    response = cognito.get_credentials_for_identity(IdentityId="arn:aws:cognito-idp:us-east-1:035742354968:userpool/us-east-1_NEm4KpQy1")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
--- js/config.js
+++ js/config.js
@@ -1,8 +1,18 @@
+//window._config = {
+//    cognito: {
+//        userPoolId: 'us-east-1_NEm4KpQy1', // e.g. us-east-2_uXboG5pAb
+//        region: 'us-east-1', // e.g. us-east-2
+//		clientId: '1vj0ibiivfq2jm102lr43bgdql'
+//    },
+//};
+
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NEm4KpQy1', // e.g. us-east-2_uXboG5pAb
-        region: 'us-east-1', // e.g. us-east-2
-		clientId: '1vj0ibiivfq2jm102lr43bgdql'
+        userPoolId: 'us-east-1_teQ5TT9Ly', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17kp1lqjkjq2lj20i1hora2u9o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
+    //api: {
+    //    invokeUrl: 'https://xnbxfpw731.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    //}
 };
-
